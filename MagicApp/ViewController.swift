93ce6fd6e114f8c935f5848e7b6bb26b864408cb
Francisco Lucena <franlucena27@gmail.com>
2023-05-20 21:57:16 +0200

    Initial Commit

---
+++ MagicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicApp
+//
+//  Created by Francisco Lucena de Juan on 20/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
