2b5472789d5cdd66ba180a2cbfe6016fc357fe5c
Francisco Lucena <franlucena27@gmail.com>
2023-05-20 22:03:42 +0200

    Add some configuration, removed UIScene and created Constants

---
+++ MagicApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MagicApp
+//
+//  Created by Francisco Lucena de Juan on 20/5/23.
+//
+
+import Foundation
+
+class Constants {
+    static let cardsURL: String = "https://api.magicthegathering.io/v1/cards"
+}
